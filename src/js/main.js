c72bbd9a9ccdf58e6b998c51a81c5056e8bbfe97
dambus <dambusns@gmail.com>
2023-03-20 13:18:18 +0100

    autopush

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,20 @@
 const mobileMenuToggle = document.querySelector(".toggle-menu");
-const mobileNavItems = document.querySelector(".mobile-nav--items");
+const mobileNavItems = document.querySelector(".mobile-nav-items");
+const mobileNavItem = document.querySelectorAll(".mobile-nav-items--item");
+const toggleCSSClasses = (el, ...cls) =>
+  cls.map((cl) => el.classList.toggle(cl));
 
 mobileMenuToggle.addEventListener("click", function () {
   mobileMenuToggle.classList.toggle("active");
-  // mobileNavItems.classList.toggle("no-opacity");
-  // mobileNavItems.classList.toggle("hidden");
-  // mobileNavItems.classList.toggle("from-right");
-  // mobileNavItems.classList.toggle("no-opacity");
-  const toggleCSSClasses = (el, ...cls) =>
-    cls.map((cl) => el.classList.toggle(cl));
-  toggleCSSClasses(mobileNavItems, "no-opacity", "hidden", "from-right");
+  toggleCSSClasses(mobileNavItems, "hidden", "no-opacity");
+  // toggleCSSClasses([...mobileNavItem], "from-right");
+  console.log(mobileNavItem);
+  mobileNavItem.forEach(function (currentValue, currentIndex, listObj) {
+    setTimeout(() => {
+      console.log("evo ga");
+      toggleCSSClasses(listObj[currentIndex], "no-opacity");
+    }, 1000);
+  });
+
+  console.log("zasto vovo ne radi?");
 });
